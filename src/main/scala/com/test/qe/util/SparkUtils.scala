2aeaf2801ce16aab8374f1781b742dc8f961fd72
johnny <a@b.com>
2018-08-18 17:03:44 +0800

    add spark utils

--- src/main/scala/com/test/qe/util/SparkUtils.scala
+++ src/main/scala/com/test/qe/util/SparkUtils.scala
@@ -1,5 +1,31 @@
 package com.test.qe.util
 
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+
 object SparkUtils {
 
+  var sparkSession:SparkSession=null
+
+  def getSparkSession():SparkSession={
+    this.synchronized{
+      if(sparkSession==null){
+        val conf = new SparkConf()
+        EnvProperty.getEntriesWithPrefix("spark").foreach(entry=>{
+          conf.set(entry._1,entry._2)
+        })
+        EnvProperty.getEntriesWithPrefix("javax").foreach(entry=>{
+          conf.set(entry._1,entry._2)
+        })
+        sparkSession=SparkSession.builder().config(conf).getOrCreate()
+      }
+    }
+    sparkSession
+  }
+
+  def getSparkContext():SparkContext={
+    getSparkSession().sparkContext
+  }
+
 }
+
