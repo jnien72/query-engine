f638cc82e4d2e78d4da589bc8a15744924b84718
johnny <a@b.com>
2018-08-18 17:42:41 +0800

    add sample spark query

--- src/main/scala/com/test/qe/QueryEngine.scala
+++ src/main/scala/com/test/qe/QueryEngine.scala
@@ -1,12 +1,23 @@
 package com.test.qe
 
-import com.test.qe.util.HadoopUtils
+import com.test.qe.util.{HadoopUtils, SparkUtils}
 
 object QueryEngine {
   def main(args:Array[String]):Unit={
     HadoopUtils.getHadoopConf()
+    val sparkSession=SparkUtils.getSparkSession()
+    val csisDf=sparkSession.read.parquet("dev/ecs/data/csis")
+    csisDf.createOrReplaceTempView("csis")
 
-    println("hi")
+    val csisDf2=sparkSession.sql(
+      "select event_date, max(client.ip_address) " +
+        "from csis " +
+        "where event_date='2018-08-17'")
 
+    csisDf2.show(1000)
+
+
+
+//    sparkSession.sql("select * from csis")
   }
 }
